54e7d2aad157a556fe8eb75d737a42438868deb1
Mark Bowling <gmarkb@hotmail.com>
2022-01-07 13:50:00 -0500

    Cleanup for live test

--- LightStage/Form1.Designer.cs
+++ LightStage/Form1.Designer.cs
@@ -50,7 +50,7 @@
             this.imageBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.imageBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.imageBox1.Location = new System.Drawing.Point(0, 0);
-            this.imageBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.imageBox1.Margin = new System.Windows.Forms.Padding(2);
             this.imageBox1.Name = "imageBox1";
             this.imageBox1.Size = new System.Drawing.Size(869, 575);
             this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -60,7 +60,7 @@
             // camera1
             // 
             this.camera1.Location = new System.Drawing.Point(291, 8);
-            this.camera1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.camera1.Margin = new System.Windows.Forms.Padding(2);
             this.camera1.Name = "camera1";
             this.camera1.Size = new System.Drawing.Size(50, 27);
             this.camera1.TabIndex = 3;
@@ -71,7 +71,7 @@
             // camera2
             // 
             this.camera2.Location = new System.Drawing.Point(345, 8);
-            this.camera2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.camera2.Margin = new System.Windows.Forms.Padding(2);
             this.camera2.Name = "camera2";
             this.camera2.Size = new System.Drawing.Size(50, 27);
             this.camera2.TabIndex = 4;
@@ -83,7 +83,7 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(641, 8);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(90, 27);
             this.button1.TabIndex = 5;
@@ -94,7 +94,7 @@
             // serialTextBox
             // 
             this.serialTextBox.Location = new System.Drawing.Point(16, 28);
-            this.serialTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.serialTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.serialTextBox.Name = "serialTextBox";
             this.serialTextBox.Size = new System.Drawing.Size(145, 20);
             this.serialTextBox.TabIndex = 6;
@@ -112,7 +112,7 @@
             // camera3
             // 
             this.camera3.Location = new System.Drawing.Point(399, 8);
-            this.camera3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.camera3.Margin = new System.Windows.Forms.Padding(2);
             this.camera3.Name = "camera3";
             this.camera3.Size = new System.Drawing.Size(50, 27);
             this.camera3.TabIndex = 8;
@@ -124,7 +124,7 @@
             // camera5
             // 
             this.camera5.Location = new System.Drawing.Point(507, 8);
-            this.camera5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.camera5.Margin = new System.Windows.Forms.Padding(2);
             this.camera5.Name = "camera5";
             this.camera5.Size = new System.Drawing.Size(50, 27);
             this.camera5.TabIndex = 10;
@@ -136,7 +136,7 @@
             // camera4
             // 
             this.camera4.Location = new System.Drawing.Point(453, 8);
-            this.camera4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.camera4.Margin = new System.Windows.Forms.Padding(2);
             this.camera4.Name = "camera4";
             this.camera4.Size = new System.Drawing.Size(50, 27);
             this.camera4.TabIndex = 9;
@@ -151,7 +151,7 @@
             this.panel1.Controls.Add(this.serialTextBox);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(869, 54);
             this.panel1.TabIndex = 11;
@@ -166,7 +166,7 @@
             this.panel2.Controls.Add(this.camera4);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel2.Location = new System.Drawing.Point(0, 538);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(869, 37);
             this.panel2.TabIndex = 12;
@@ -181,10 +181,10 @@
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.imageBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "LightStage  1.2";
+            this.Text = "LightStage  1.3";
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
