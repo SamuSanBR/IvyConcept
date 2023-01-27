2f2530d3b411304698fff1c083328cd812d71a12
Samuel Cassan <samuel.cassan@ivytech.com>
2023-01-26 21:11:34 -0300

    FormDesignerCerto

--- LightStage/Form1.Designer.cs
+++ LightStage/Form1.Designer.cs
@@ -111,6 +111,7 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(3798, 163);
             this.panel1.TabIndex = 11;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // btnSair
             // 
